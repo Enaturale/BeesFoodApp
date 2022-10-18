56d828c2e673bf0f9bab31f1e7d4d88d998234d0
Enaturale <enaturaleb@gmail.com>
2022-10-18 11:18:54 +0100

    Drawer Navigation problem

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -3,6 +3,35 @@ import React from 'react';
 import { View, Text, Pressable, StyleSheet } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useNavigation } from '@react-navigation/native';
+
+// import {createDrawerNavigator} from "@react-navigation/drawer"
+// // import {createAppContainer} from '@react-navigation'
+
+// // import Profile from "../Screens/Profile/Profile";
+// // import Cart from "../Screens/Cart/Cart";
+// // import CategoriesPage from "../Screens/CategoriesPage/CategoriesPage";
+
+// import Profile from '../../Screens/Profile/Profile';
+// import Cart from '../../Screens/Cart/Cart';
+// import CategoriesPage from '../../Screens/CategoriesPage/CategoriesPage';
+
+// const Drawer = createDrawerNavigator({
+//     Profile,
+//     Cart,
+//     CategoriesPage
+// });
+
+// const DrawerTab = () => {
+//     return(
+//         <Drawer.Navigator>
+//             <Drawer.Screen name="Categ"  component={CategoriesPage} />
+//             <Drawer.Screen name="Cartio" component={Cart} />
+//             <Drawer.Screen name="prof" component={Profile}/>
+
+//         </Drawer.Navigator>
+//     )
+// }
+
 const Header = ( ) => {
     const navigation = useNavigation();
     const gotoProfile = () =>{
@@ -11,7 +40,7 @@ const Header = ( ) => {
     return (
         <View style={{marginTop: 27, flexDirection:'row', justifyContent:'space-between', width: 340,}}>
             <View >
-                <Pressable style={Styles.pressable}>
+                <Pressable style={Styles.pressable} >
                     <MaterialIcons name="menu" size={40} color='#FF9B07' />
                 </Pressable>
             </View>
---
+++ Navigation/DrawerTab.js
@@ -0,0 +1,27 @@
+// import React from React;
+
+// import {createDrawerNavigator} from "@react-navigation/drawer"
+
+
+// import Profile from "../Screens/Profile/Profile";
+// import Cart from "../Screens/Cart/Cart";
+// import CategoriesPage from "../Screens/CategoriesPage/CategoriesPage";
+
+// const Drawer = createDrawerNavigator({
+//     Profile,
+//     Cart,
+//     CategoriesPage
+// });
+
+// const DrawerTab = () => {
+//     return(
+//         <Drawer.Navigator>
+//             <Drawer.Screen name="Categ"  component={CategoriesPage} />
+//             <Drawer.Screen name="Cartio" component={Cart} />
+//             <Drawer.Screen name="prof" component={Profile}/>
+
+//         </Drawer.Navigator>
+//     )
+// }
+
+// export default DrawerTab;
