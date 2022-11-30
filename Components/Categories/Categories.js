c27b37fe889a9a0e22f6ba938d48e01999e17987
Enaturale <enaturaleb@gmail.com>
2022-11-30 16:36:23 +0100

    Made a beautiful Flatlist

--- Components/Categories/Categories.js
+++ Components/Categories/Categories.js
@@ -14,27 +14,32 @@ const data = [
     {
         id: '1',
         image: 'https://cdn.pixabay.com/photo/2021/08/21/23/55/pizza-6563825__340.png',
-        title: 'Pizza'
+        title: 'Pizza',
+        types:'20 types'
     },
     {
         id: '2',
         image: 'https://cdn.pixabay.com/photo/2020/10/22/08/56/donut-5675340__340.png',
         title: 'Snacks',
+        types:'20 types'
     },
     {
         id: '3',
         image: 'https://cdn.pixabay.com/photo/2021/05/14/23/35/breakfast-6254633__340.png',
         title: 'Salads',
+        types:'20 types'
     },
     {
         id: '4',
         image: 'https://cdn.pixabay.com/photo/2021/05/23/22/27/sandwich-6277424__340.png',
-        title: 'Light',
+        title: 'Light Meals',
+        types:'20 types'
     },
     {
         id: '5',
         image: 'https://cdn.pixabay.com/photo/2012/04/14/14/29/wine-34125__340.png',
         title: 'Drinks',
+        types:'20 types'
     }
 
 ]
--- Screens/CategoriesPage/CategoriesPage.js
+++ Screens/CategoriesPage/CategoriesPage.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import { 
+import {
     ScrollView,
-    View, 
-    Text, 
-    StyleSheet, 
-    FlatList, 
-    Pressable, 
+    View,
+    Text,
+    StyleSheet,
+    FlatList,
+    Pressable,
     Image,
-     SafeAreaView } from "react-native"
+    SafeAreaView
+} from "react-native"
 import BackButton from '../../Components/BackButton/BackButton'
 import FontsPage from '../../Components/FontsPage/FontsPage'
 import Searchbar from '../../Components/Searchbar/Searchbar';
@@ -16,27 +17,32 @@ const data = [
     {
         id: '1',
         image: 'https://cdn.pixabay.com/photo/2021/08/21/23/55/pizza-6563825__340.png',
-        title: 'Pizza'
+        title: 'Pizza',
+        types: '20 types',
     },
     {
         id: '2',
         image: 'https://cdn.pixabay.com/photo/2020/10/22/08/56/donut-5675340__340.png',
         title: 'Snacks',
+        types: '20 types',
     },
     {
         id: '3',
         image: 'https://cdn.pixabay.com/photo/2021/05/14/23/35/breakfast-6254633__340.png',
         title: 'Salads',
+        types: '20 types',
     },
     {
         id: '4',
         image: 'https://cdn.pixabay.com/photo/2021/05/23/22/27/sandwich-6277424__340.png',
         title: 'Light',
+        types: '20 types',
     },
     {
         id: '5',
         image: 'https://cdn.pixabay.com/photo/2012/04/14/14/29/wine-34125__340.png',
         title: 'Drinks',
+        types: '20 types',
     }
 
 ]
@@ -48,13 +54,25 @@ const CategoriesPage = ({ navigation }) => {
         navigation.navigate('Dash')
     }
 
-    const items = ({ item }) => {
+    const items = ({ item, index }) => {
+        const isEnd = index === data.length - 1;
         return (
             <View>
                 <Pressable style={Styles.itemsContainer}>
-                    <Image source={{uri: item.image}} style={Styles.image} />
-                    <Text style={{ color: 'white' }}>{item.title}</Text>
-                </Pressable>                
+                    <View style={{ flexDirection: 'row', width: 320, justifyContent: 'space-between' }}>
+                        <View>
+                            <Image source={{ uri: item.image }} style={Styles.image} />
+                        </View>
+                        <View style={{ marginRight: 20, }}>
+                            <Text style={Styles.text} numberOfLines={2}>{item.title}</Text>
+                            <Text style={Styles.secondtext}>{item.types}</Text>
+                        </View>
+
+                        <Pressable style={Styles.pressable} >
+                            <Text style={Styles.pressableText}>See All</Text>
+                        </Pressable>
+                    </View>
+                </Pressable>
             </View>
         )
     }
@@ -68,6 +86,7 @@ const CategoriesPage = ({ navigation }) => {
 
                 <View style={{ marginTop: 5 }}>
                     <Text style={Styles.title}>Categories</Text>
+
                 </View>
             </View>
 
@@ -75,19 +94,18 @@ const CategoriesPage = ({ navigation }) => {
                 <Searchbar />
             </View>
 
-        
+            <View style={{height: '85%'}}>
                 <FlatList
                     data={data}
                     renderItem={items}
+                    keyExtractor={items => items.id}
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                    // ListFooterComponent={() => <Text>End of List</Text>}
 
                 />
+            </View>
 
-                <View style={{marginTop: 400}}>
-
-                </View>
-
-
-           
         </SafeAreaView>
     )
 }
@@ -107,13 +125,39 @@ const Styles = StyleSheet.create({
         height: 120,
         backgroundColor: 'black',
         marginTop: 20,
-        justifyContent:'center',
+        justifyContent: 'center',
         borderRadius: 15,
 
     },
-    image:{
+    image: {
+        height: 70,
+        width: 80,
+        marginHorizontal: 20,
+    },
+    text: {
+        color: 'white',
+        fontFamily: 'SegoeItalic',
+        fontSize: 20,
+    },
+    secondtext: {
+        color: '#FF9B07',
+        fontSize: 17,
+    },
+    pressable: {
+        width: 80,
         height: 50,
-        width: 50,
+        borderColor: '#FF9B07',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2,
+        borderRadius: 15,
+
+    },
+    pressableText: {
+        color: '#FF9B07',
+        fontSize: 15,
+        fontFamily: 'Segoe'
+
     }
 })
 
